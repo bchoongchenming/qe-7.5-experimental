401d370303a604ccbe1a00be84374ffea3876d4b
bchoongchenming <choongchenming@gmail.com>
2026-01-14 17:24:08 +0800

    Init from https://gitlab.com/QEF/q-e branch qe-7.5

---
+++ q-e/external/qe-gipaw/src/gipaw_version.f90
@@ -0,0 +1,20 @@
+!
+! Copyright (C) 2003-2013 Quantum ESPRESSO group
+! This file is distributed under the terms of the
+! GNU General Public License. See the file `License'
+! in the root directory of the present distribution,
+! or http://www.gnu.org/copyleft/gpl.txt .
+!
+
+! NOTE: this file is automatically created
+
+!----------------------------------------------------------------------------
+MODULE gipaw_version
+  !
+  IMPLICIT NONE
+  !
+  SAVE
+  !
+  CHARACTER (LEN=40) :: gipaw_git_revision = "4c7640a982d6834e7ed61cb360211192f8295ad3"
+  !
+END MODULE gipaw_version
