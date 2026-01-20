5f51aca23e4a6dacb17ed680eaefc719e9284a34
bchoongchenming <choongchenming@gmail.com>
2026-01-20 17:48:27 +0800

    Added: Si_hse

--- experiments/run_nsys_profiling.sh
+++ experiments/run_nsys_profiling.sh
@@ -1,8 +1,9 @@
 WORK_DIR=$(dirname $0)
 
 NP=2
-NAME="Al2O3_3x3x1"
+# NAME="Al2O3_3x3x1"
 # NAME="AUSURF112"
+# NAME="Si_hse"
 EXPT_NAME="$(date +"%Y-%m-%d")_${NAME}_ngpus$(printf "%02d" $NP)"
 
 cd $WORK_DIR/$NAME &&
